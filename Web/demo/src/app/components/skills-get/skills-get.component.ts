53e7c2e127405fedd1433abec5cfe53fab8e95f4
akplogman <austinkyle14@gmail.com>
2020-03-25 21:22:11 -0500

    updated get skill componenta

--- Web/demo/src/app/components/skills-get/skills-get.component.ts
+++ Web/demo/src/app/components/skills-get/skills-get.component.ts
@@ -1,19 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, PipeTransform } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { FormControl } from '@angular/forms';
+
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 import { SkillService } from 'src/app/services/skill.service';
 
+interface Skill {
+  id?: number,
+  SkillName: string;
+}
 
 @Component({
   selector: 'app-skills-get',
   templateUrl: './skills-get.component.html',
-  styleUrls: ['./skills-get.component.css']
+  styleUrls: ['./skills-get.component.css'],
+  providers: [DecimalPipe]
 })
-export class SkillsGetComponent {
-  skills: Array<{}>
+export class SkillsGetComponent implements OnInit {
+  skills: Array<{id?: number, SkillName: string}>
   page = 1;
   pageSize = 4;
   collectionSize = null
 
-  constructor(private skillService: SkillService) { }
+  filter = new FormControl('');
+
+  constructor(private skillService: SkillService, pipe: DecimalPipe) { 
+    if(this.skills) {
+    this.filter.valueChanges.pipe(
+      startWith(''),
+      map(text => this.search(text, pipe))
+    );
+    }
+  }
 
   ngOnInit() {
     this.getSkills()
@@ -27,10 +46,21 @@ export class SkillsGetComponent {
       })
   }
 
+  search(text: string, pipe: PipeTransform) {
+    if(this.skills) {
+    return this.skills.filter(skill => {
+      const term = text.toLowerCase();
+      return skill.id
+          || pipe.transform(skill.SkillName).includes(term)
+    });
+  }
+  }
+
   get listSkills() {
-    return this.skills
-      .map((skill, i) => ({id: i + 1, ...skill}))
-      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+    if(this.skills) {
+    return this.skills.map((skill, i) => ({id: i + 1, ...skill}))
+    .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+    }
   }
 
 
